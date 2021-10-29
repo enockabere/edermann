e21ff7762aee3565192b2ecbd6145dca763c3b60
enockabere <maebaenock95@gmail>
2021-10-29 18:48:31 +0300

    create block template

--- booking/urls.py
+++ booking/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.landing, name="landing"),
     path('sketch',views.sketch,name="sketch"),
+    path('block',views.block,name="block"),
+    path('floor',views.floor,name="floor"),
 ]
--- booking/views.py
+++ booking/views.py
@@ -5,3 +5,7 @@ def landing(request):
     return render(request,'main/landing.html')
 def sketch(request):
     return render(request,'main/sketch.html')
+def block(request):
+    return render(request,'main/block.html')
+def floor(request):
+    return render(request,'main/floor')
