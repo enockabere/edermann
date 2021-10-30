6cd2c8f9de44bda89a0fa13025d7df1d53d2799c
enockabere <maebaenock95@gmail>
2021-10-30 14:14:39 +0300

    create terms and conditions

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('auth/', views.login_request, name='login'),
+    # path('acc', views.account_request,name="acc")
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def login_request(request):
     return render(request,'accounts/auth.html')
+# def account_request(request):
+#     return render(request,'accounts/account.html')
