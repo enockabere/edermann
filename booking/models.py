0a6bc08a8a6c8c83c7cc981700d17ea8ac136981
enockabere <maebaenock95@gmail>
2021-11-01 13:38:09 +0300

    create apartments model

--- booking/models.py
+++ booking/models.py
@@ -7,7 +7,12 @@ class Estate(models.Model):
     name = models.CharField( verbose_name="Estate Name" ,max_length=200, unique=True)
     logo = CloudinaryField('image')
     about = TextField(verbose_name="About Estate")
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField(auto_now_add=True,verbose_name="Date Added")
     
     def __str__(self):
         return str(self.name)
+class Apartment(models.Model):
+    name = models.CharField(verbose_name="Apartment Name", max_length=200,unique=True)
+    image = CloudinaryField('image')
+    date = models.DateTimeField(auto_now_add=True,verbose_name="Date Added")
+    estate = models.ForeignKey(Estate,on_delete=models.CASCADE,related_name="estate_name")
