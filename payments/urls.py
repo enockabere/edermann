3aecdab40c8d9b74164edd06a5a39e3669f80db8
enockabere <maebaenock95@gmail.com>
2022-09-30 09:18:46 +0300

    create env variables

--- payments/urls.py
+++ payments/urls.py
@@ -1,6 +1,6 @@
-from django.urls import  path
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('pay',views.payments,name='pay'),
+    path('pay', views.Payments.as_view(), name='pay'),
 ]
--- payments/views.py
+++ payments/views.py
@@ -1,5 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views import View
+from django_daraja.mpesa.core import MpesaClient
+from django.contrib import messages
+
 
 # Create your views here.
-def payments(request):
-    return render(request, 'main/payment.html')
+cl = MpesaClient()
+stk_push_callback_url = 'https://darajambili.herokuapp.com/express-payment'
+
+
+class Payments(View):
+    def get(self, request):
+        return render(request, 'main/payment.html')
+
+    def post(self, request):
+        try:
+            if request.method == 'POST':
+                token = cl.access_token()
+                phone_number = request.POST.get('phone_number')
+                amount = int(request.POST.get('amount'))
+                account_reference = request.POST.get('account_reference')
+                transaction_desc = 'Description'
+
+                callback_url = stk_push_callback_url
+                r = cl.stk_push(
+                    phone_number, amount, account_reference, transaction_desc, callback_url)
+                messages.success(request, r.response_description)
+                print(r)
+                return redirect('pay')
+        except Exception as e:
+            print(e)
+            return redirect('pay')
